2d2b3ffbf7fb1c11cb4d97197ad4bb43137851b4
arnodmental <demental@sat2way.com>
2011-05-12 21:20:26 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmtrails::Application.config.secret_token = '40c95d52533a86116120808b99c3d6d741fdac3333294c269a0a949239d4e4c171a385865c8b7849a2a559d7e42a5b5f860ab162c301210f9cfbae5c8e08e6c0'

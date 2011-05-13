2f6143d404130092e35c072f8f467f0455ac2e69
arnodmental <demental@sat2way.com>
2011-05-13 19:10:38 +0200

    Chapter 3 finish

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,6 +7,10 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content=>" | Home")
+    end  
   end
 
   describe "GET 'about'" do
@@ -14,6 +18,10 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content=>" | About")
+    end  
   end
 
   describe "GET 'contact'" do
@@ -21,6 +29,10 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content=>" | Contact")
+    end  
   end
 
 end
